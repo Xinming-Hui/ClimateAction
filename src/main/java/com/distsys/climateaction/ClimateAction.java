5005b054e8f49bf55fe1bd39b6bc2c01511042c4
Xinming-Hui <simon.hui.ireland@gmail.com>
2025-07-30 11:35:57 +0100

    feat: init project

---
+++ src/main/java/com/distsys/climateaction/ClimateAction.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.distsys.climateaction;
+
+/**
+ *
+ * @author xinminghui
+ */
+public class ClimateAction {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
